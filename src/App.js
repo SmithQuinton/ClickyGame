26935ce935181a62139b26527163e1420aab5eac
Quinton Smith <smith.quinton42@gmail.com>
2019-09-10 12:42:41 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import cards from "./cards.json";
-import "./App.css";
+// import "./App.css";
 
 class App extends Component {
   // Setting this.state.cards to the cards json array
